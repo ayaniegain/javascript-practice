d765a0f4d94ee3b9087999fd32120a48baa15d3a
ayaniegain <ayaniegain@gmail.com>
2023-08-30 08:51:20 +0530

    feat : pallindrome done

--- dsa_practice.js
+++ dsa_practice.js
@@ -1,17 +1,6 @@
 // pallindrome
-// let value =121
-
-// let valArry=value.toString().split('').reverse().join("")
-
-// if (parseInt(valArry)===value) {
-//     console.log("pall")
-// }else{
-//     console.log("not pal")
-// }
-
-// (valArry==value)?(console.log("pallindrome")): (console.log("not pallindrome"))
 
 function isPallindrome(val) {
     return val<0? false: (val==+val.toString().split('').reverse().join("")) 
 }
-console.log(isPallindrome(121))
+console.log(isPallindrome(120))
