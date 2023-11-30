913cb5583096c4cc2e5aa9e50e52ef5870be0243
ayaniegain <ayaniegain@gmail.com>
2023-11-30 18:04:01 +0530

    cc

--- JS_concept.js
+++ JS_concept.js
@@ -1,1 +1,28 @@
+// IIFE
+
+
+// (function () {
+//    console.log("hello");
   
+//  })()
+
+//promise
+// let res= new Promise((resolve,reject)=>{
+//   if (resolve) {
+//     resolve ("success");
+//   }
+ 
+
+// })
+// res.then((e)=>{
+// console.log(e);
+// }
+
+// ).catch((e)=>{
+//   console.log(e);
+// }
+  
+// )
+
+
+
