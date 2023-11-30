ae3fb86305e4cdbefb2c2d7cb50b343cc6de17fd
ayaniegain <ayaniegain@gmail.com>
2023-11-30 20:21:38 +0530

    ccv

--- JS_concept.js
+++ JS_concept.js
@@ -1,9 +1,8 @@
 // IIFE
 
-
 // (function () {
 //    console.log("hello");
-  
+
 //  })()
 
 //promise
@@ -11,7 +10,6 @@
 //   if (resolve) {
 //     resolve ("success");
 //   }
- 
 
 // })
 // res.then((e)=>{
@@ -21,8 +19,61 @@
 // ).catch((e)=>{
 //   console.log(e);
 // }
-  
+
 // )
 
+// fetch api call
 
+// function apicall() {
+//  let res=   fetch('https://randomuser.me/api/')
+//    res.then((e)=>{
+//       return e.json()
+//     })
+//     .then((e)=>{
+//         console.log(e);
+//     })
+//     .catch((e)=>{
+//         console.log(e);
+//     })
 
+//     // console.log(res);
+// }
+
+// apicall()
+
+// with try catch
+// try {
+// } catch (error) {}
+
+// function apicall() {
+//   try {
+//     let res = fetch("https://randomuser.me/api/");
+//     res
+//       .then((e) => {
+//         return e.json();
+//       })
+//       .then((e) => {
+//         console.log(e);
+//       });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// apicall();
+
+ //callback
+
+//  function outer(a,callback) {
+
+//      callback()
+//      console.log(a+b);
+
+    
+//  }
+
+//  function inner() {
+
+//     return b=4
+// }
+// outer(2,inner)
