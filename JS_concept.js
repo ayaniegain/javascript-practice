4f8e0b494e7a659c70b7986ba850936e25779c57
ayaniegain <ayaniegain@gmail.com>
2023-12-01 09:57:01 +0530

    cc

--- JS_concept.js
+++ JS_concept.js
@@ -77,3 +77,172 @@
 //     return b=4
 // }
 // outer(2,inner)
+
+//closoure
+
+// let val=28
+// function outer() {
+//   return function inner() {
+//     //  val=3 
+
+//      return val
+
+
+        
+//     }
+// }
+
+// let result=outer()
+
+// let res=result()
+
+// console.log(res);
+
+
+// call apply bind 
+
+// //call
+// function greet(name) {
+// console.log(`this is ${name} my ${this.title}`);
+    
+// }
+// const context={title:'hello'};
+
+// greet.call(context,'User')
+
+// //apply
+
+// function greet2(name) {
+// console.log(`this is ${name} my ${this.title}`);
+    
+// }
+// const context2={title:'hello'};
+
+// greet2.apply(context2,["User2"])
+
+// //bind
+
+// class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Person {
+
+    constructor(name,age) {
+
+        this.name=name;
+        this.age=age
+
+    }
+
+    canrun(){
+        console.log(`hello  i am ${this.name} my age is ${this.age}`);
+    }
+ 
+}
+
+let value1= new Person("ayan",23)
+
+value1.canrun()
+
+console.log(value1);
